e2472143fd40720aa91f4d565c45a728329d1b8c
Nathan <nathan.vincent.2.718@gmail.com>
2021-12-07 23:16:59 -0600

    add image generation method

--- MapGenerator.py
+++ MapGenerator.py
@@ -1,5 +1,17 @@
+from PIL import Image
+import requests
+
 def generateStaticImage(lat, lon, width, height):
-    print('Generating image for',lat,",",lon,'of size',width,'by',height)
+    key = open("geokey.key").read()
+    
+    baseString = ('http://www.mapquestapi.com/staticmap/v5/map?' 
+    + 'key=' + str(key) + '&type=map&size=' + str(width) + ',' 
+    + str(height) + '&locations=' + str(lat) + ',' + str(lon) 
+    + '%7Cmarker-sm-50318A-1&scalebar=true&zoom=15&rand=286585877')
+
+    im = Image.open(requests.get(baseString, stream=True).raw)
+    im.show()
 
 if __name__ == '__main__':
     generateStaticImage(33.449945,-88.781702,1000,1000)
+    
