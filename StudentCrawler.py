5fda09be65ea070bafb4a2b63ca9c0bebffe413c
Nathan <nathan.vincent.2.718@gmail.com>
2022-02-12 10:23:45 -0600

    comment StudentCrawler.py

--- StudentCrawler.py
+++ StudentCrawler.py
@@ -38,143 +38,164 @@ alphas = ['a', 'b', 'c', 'd', 'e', 'f', 'g',
 # the filename to output the results to
 firstFileName = ""
 
-def crawl():
+def crawl(studentMode = True):
     """
     Webscrapes from the MSU student directory using selenium to find 
     all students in the database and save their information to a file.
     """
 
     try:
+        # ensure selenium driver exists
         print("Begining scraping sequence")
         exe = os.path.exists(PATH)
 
         if exe:
             print("Executable found")
 
+            # create driver object based on seleium exe
             driver = webdriver.Chrome()
             driver.get("https://my.msstate.edu/")
 
-            elem = driver.find_element(By.ID,USERNAME_ID)
+            # find the CAS username field and input our username
+            elem = driver.find_element(By.ID, USERNAME_ID)
             elem.clear()
             elem.send_keys(INJECTION_NAME)
 
+            # find the CAS password field and input our password
             elem = driver.find_element(By.ID, PASSWORD_ID)
             elem.clear()
             elem.send_keys(INJECTION_PASSWORD)
 
+            # find and click teh submit button
             driver.find_element(By.NAME,'submit').click()
 
-            #DUO handling
+            # wait for DUO to load
             masterElemnString = "login"
             myElem = WebDriverWait(driver, 10).until(
                 EC.presence_of_element_located((By.ID, masterElemnString)))
             print(f"DUO {masterElemnString} loaded")
 
-            #switch to duo iFrame
+            # switch to the DUO iFrame
             iFrameTitle = "duo_iframe"
             driver.switch_to.frame(iFrameTitle)
 
-            #wait for push button to load and click it
+            # wait for the push button to load and click it
             pushButtontext = "Send Me a Push"
             WebDriverWait(driver, PUSH_TIMEOUT).until(
                 EC.presence_of_element_located((By.XPATH, f"//*[contains(text(), '{pushButtontext}')]"))).click()
             print("Push sent")
 
-            #wait for directory to load
+            # wait for the student directory element to load
             directoryID = "portlet_MSUDirectory1612_WAR_directory1612"
             WebDriverWait(driver, 10).until(
                 EC.presence_of_element_located((By.ID, directoryID)))
             print("Directory element loaded")
 
-            #select student radio button
-            driver.find_element(By.XPATH, "//input[@value='s']").click()
+            personType = 's' if studentMode else 'f'
 
-            #generate permutation lists
+            # select student/faculty RadioButton
+            driver.find_element(By.XPATH, f"//input[@value='{personType}']").click()
+
+            # generate permutations to loop through
             firsts = generatePairsList()
             lasts = generatePairsList()
 
-            #skip permutations we have finished
+            # skip permutations we have already performed
             skips = 0
             skipped = count_files('FirstsParsed')
-            print('skipped:',skipped)
+            print('skipped:', skipped)
 
+            # outer loop for first permutations, inner loop implies a runtime of O(n^2)
             for firstIndex in range(len(firsts)):
                 if skips != skipped:
-                    print("Skipping: first =",firsts[firstIndex])
+                    print("Skipping: first =", firsts[firstIndex])
                     skips += 1
                     continue
 
-                #Create the file we're going to write to for this first pair
+                # create the file we're going to write to for the firstname permutation
                 global firstFileName
                 firstFileName = "Firsts/First_Name_Contains_" + firsts[firstIndex] + ".txt"
+
+                # open our file with overwrite permissions
                 file = open(firstFileName,'w+')
                 file.close()
 
+                # inner loop for last permutations
                 for last in lasts:
                     try:
-                        #inect last name search
+                        # inject the lastname string into the field
                         firstSearchFieldID = 'fld1_search_term'
                         elem = WebDriverWait(driver, 10).until(
                             EC.presence_of_element_located((By.ID, firstSearchFieldID)))
                         elem.clear()
                         elem.send_keys(last)
 
-                        #inject first name search
+                        # inject the firstname string into the field
                         secondSearchFieldID = 'fld2_search_term'
                         elem = WebDriverWait(driver, 10).until(
                             EC.presence_of_element_located((By.ID, secondSearchFieldID)))
                         elem.clear()
                         elem.send_keys(firsts[firstIndex])
 
-                        #Submit search based on provided first and last name
+                        # click submit 
                         searchText = "submit"
                         myElem = WebDriverWait(driver, 10).until(
                             EC.element_to_be_clickable((By.ID, searchText)))
                         myElem.click()
 
-                        #in case we're changing seraches wait for this to settle
-                        time.sleep(1)
+                        # wait for results to load
+                        time.sleep(0.5)
 
-                        #Get how many pages were returned for this query
+                        # find the number of students returned
                         countElement = WebDriverWait(driver, 10).until(
                             EC.element_to_be_clickable((By.ID, 'count')))
                         elementCount = int(re.search(r'\d+', countElement.get_attribute('innerHTML')).group())
+
                         print("---------------------------------")
                         print("Querry: first =",firsts[firstIndex],"last =",last)
                         print('Records returned:', elementCount)
+
+                        # find the number of pages
                         numPages = int(math.ceil(elementCount / 10.0))
                         print('Number of pages:',numPages)
                         print("---------------------------------")
 
-                        #immediately continue of query returned no results
+                        # if no results returned, continue
                         if elementCount == 0:
                             continue
 
-                        #for all pages returned from this query
+                        # loop through pages returned from this query
                         page = 0
                         while page < numPages + 1:
-                            #get information for each person on current page
+                            # get information for each person on current page
                             for person in driver.find_elements(By.CLASS_NAME, "person"):
                                 printPersonDetails(driver, person, 
                                     person.get_attribute('innerHTML'))
 
-                            #if the pagenumber element exists, execute js to go to the next page
+                            # if the pagenumber element exists, execute the 
+                            # javascript function to go to the next page
                             if elementExists(driver, 'pagenums'):
                                 driver.execute_script("getDetails(0)") 
                                 
-                            #inc pages
+                            # inc page counter
                             page += 1
 
+                            # sleep an adequate time to avoid spamming the backend
                             time.sleep(PAGE_SLEEP_TIMEOUT) 
 
+                        # sleep even more time
                         time.sleep(QUERRY_SLEEP_TIMEOUT)
-                    except:
+                    except Exception as e:
+                        # we don't care about errors here
                         continue
 
+                # finished with 1/26th of the permutations
                 print(firstFileName,' finished with all last permutations. Continuing to next first permutation')
 
+            # final closing message
             print('All permutations from first and last have been executed; exiting scraper')
         else:
+            # inform user where to get a selenium driver
             print("Executable not found, download from: https://sites.google.com/chromium.org/driver/downloads?authuser=0")
     except Exception as e:
         print("Exception:", e)
@@ -195,10 +216,12 @@ def printPersonDetails(driver, person, personName):
     Finds person details from the provided person html.
     '''
 
+    # init data to find
     classification = "NULL"
     major = "NULL"
 
     try:
+        # parse html tags to find data above
         parent = person.find_element(By.XPATH, "..").find_element(By.XPATH, "..")
         pTags = parent.find_elements(By.CSS_SELECTOR, "p")
         classification = pTags[1].get_attribute('innerHTML')
@@ -206,22 +229,24 @@ def printPersonDetails(driver, person, personName):
     except:
         pass
 
+    # if no data found, set to NULL
     if len(classification) == 0:
-        classification = ""
+        classification = "NULL"
     if len(major) == 0:
-        major = ""
+        major = "NULL"
 
-    #Enter person details section
+    # enter the expanded details section
     person.send_keys(Keys.ENTER)
 
-    #wait for details section to be loaded
+    # wait for the section to load
     detailsID = 'details-section'
     detailsElement = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.ID, detailsID)))
 
+    # parse the html of the returned data section
     parseHTML(detailsElement.get_attribute('innerHTML'), personName, classification, major)
 
-    #go back to query results
+    # click back to return to the returned student results
     WebDriverWait(driver, 10).until(
         EC.presence_of_element_located ((By.ID, 'back'))).send_keys(Keys.ENTER)
 
@@ -240,7 +265,7 @@ def parseHTML(studentDetails, name, classification, major):
     Parses the html and found details from the student html.
     '''
 
-    #user attributes
+    # user attributes
     studentName = name
     studentEmail = "NULL"
     studentHomePhone = 0
@@ -248,25 +273,24 @@ def parseHTML(studentDetails, name, classification, major):
     studentCampusAddress = []
     studentHomeAddress = []
    
-    soup = BeautifulSoup(studentDetails, features="html.parser")
-    pretty = soup.prettify()
-    lines = pretty.split('\n')
-
     mode = "none"
     
-    for line in lines:
-        #parse away b tags
+    # this is a bad way to parse the html but whatever
+    for line in BeautifulSoup(studentDetails, features="html.parser").prettify().split('\n'):
+        # parse away bold tags
         if "<b>" in line or "</b>" in line or "<b/>" in line:
             continue
-        #parse away break tags
+        # parse away break tags
         elif "<br>" in line or "<br/>" in line or "</br>" in line:
             continue
-        #parse away a tags
+        # parse away a tags
         elif "</a>" in line or "<a" in line or "<a/>" in line:
             continue
 
+        # parse away non-ascii chars within the current line
         line = parseNonAscii(line)
 
+        # figure out what the line contains
         if line.strip() == "Email":
             mode = "email"
             continue
@@ -280,42 +304,50 @@ def parseHTML(studentDetails, name, classification, major):
             mode = "ha"
             continue
 
-        #precautionary continue for empty lines
+        # if nothing to parse
         if len(line.strip()) < 3:
             continue
 
+        # based on mode, set the data attribute
         if mode == "email" and line.strip() != "Email":
             studentEmail = line.strip()
+        
+        # campus or home phone
         elif mode == "phone" and line.strip() != "Phone":
             if "Campus" in line:
                 studentCampusPhone = ''.join(c for c in line if c.isdigit())
             else:
                 studentHomePhone = ''.join(c for c in line if c.isdigit())
+
+        # campus address
         elif mode == "ca":
             studentCampusAddress.append(line.strip())
+        
+        # home address
         elif mode == "ha":
             studentHomeAddress.append(line.strip())
 
-    #join address lines
+    # join addresses with associated data
     studentCampusAddress = " ".join(studentCampusAddress)
     studentHomeAddress = " ".join(studentHomeAddress)
 
-    #null checks
+    # empty phone number checks
     if studentHomePhone == 0:
-        studentHomePhone = "NULL"
+        studentHomePhone = "0"
     if studentCampusPhone == 0:
-        studentCampusPhone = "NULL"
+        studentCampusPhone = "0"
     
+    # empty address checks
     if len(studentCampusAddress.strip()) == 0:
         studentCampusAddress = "NULL"
-
     if len(studentHomeAddress.strip()) == 0:
         studentHomeAddress = "NULL"
 
-    #comma checks
+    # comma checks as to not break our csv
     studentHomeAddress = studentHomeAddress.replace(",", "")
     studentCampusAddress = studentCampusAddress.replace(",", "")
 
+    # write user object to file
     file_object = open(firstFileName, 'a')
     file_object.write(studentName + ",")
     file_object.write(classification + ",")
