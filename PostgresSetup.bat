3bd8ca4a963eb364b5082e12f491a405be9d7b55
Nathan <nathan.vincent.2.718@gmail.com>
2022-04-09 18:16:31 -0500

    add pythonsetup.bat and pass

--- PostgresSetup.bat
+++ PostgresSetup.bat
@@ -20,35 +20,17 @@ docker volume create student-scraper-postgres
 ECHO Running container and binding to port 5432
 docker run -d --name=student-scraper-postgres -p 5432:5432 -v postgres-volume:/var/lib/postgresql/data -e POSTGRES_PASSWORD=1234 postgres
 
-ECHO Timing out for 5s to allow database to startup
+ECHO Sleeping for 10s to allow database to startup
 
-:: this is a hack but it works :P
-ping 127.0.0.1 -n 6 > nul
+ping 127.0.0.1 -n 11 > nul
 
 set db_name=msu_%semester%_%yy%
 ECHO Creating database with name %db_name%
 
 docker exec -it student-scraper-postgres psql -U postgres -c "CREATE DATABASE %db_name%;"
 
-ECHO Creating a virtual environment for python
+ECHO Database created, passing off to PythonSetup.bat
 
-python -m venv venv
-
-ECHO Entering the virtual environment
-
-.\venv\Scripts\activate.bat
-
-ECHO Installing requirements from requirements.txt
-
-pip install -r requirements.txt
-
-ECHO Creating tables using a python connection
-
-python .\CreateTables.py
-
-ECHO Contents of the students table of the %db_name% database
-docker exec -it student-scraper-postgres psql -U postgres -c "\d+ students"
-
-ECHO Completed pg db and table setup, you may proceed to Poster.py
+.\PythonSetup.bat
 
 EXIT
---
+++ PythonSetup.bat
@@ -0,0 +1,26 @@
+@echo off
+
+ECHO Creating a virtual environment for python
+
+python -m venv venv
+
+ECHO Sleeping for 10 seconds
+
+ping 127.0.0.1 -n 11 > nul
+
+ECHO Entering the virtual environment
+
+CALL .\venv\Scripts\activate.bat
+
+ECHO Installing requirements from requirements.txt
+
+pip install -r requirements.txt
+
+ECHO Creating students table
+
+python .\CreateTables.py
+
+ECHO Contents of the students table
+docker exec -it student-scraper-postgres psql -U postgres -c "\d+ students"
+
+ECHO Completed pg db and table setup, you may proceed to Poster.py directly as you are in the venv currently
